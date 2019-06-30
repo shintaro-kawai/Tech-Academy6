494cdc9c02c8dfe7e2d66cf7a841f586b5ed5863
shusuke.ota <shu.ota0812@gmail.com>
2019-06-30 14:03:15 +0900

    初期コミット

---
+++ Instagram/Const.swift
@@ -0,0 +1,35 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by Shusuke Ota on 2019/06/29.
+//  Copyright © 2019 shusuke. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let PostPath = "posts"
+}
+
+/*
+class Const: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+*/
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
